6ebd2cc8ea412570885a42808e76a679deaf2010
Kayateia <github@kayateia.net>
2018-04-01 21:25:14 -0700

    Add really rudimentary support for voxel types

--- src/main/scala/net/kayateia/flowerbox/client/Chunk.scala
+++ src/main/scala/net/kayateia/flowerbox/client/Chunk.scala
@@ -21,34 +21,36 @@ class Chunk {
 		val zMax = 5f
 		val zSize = xMax - zMin
 		val perlin = new Perlin()
-		for (x <- 0 to 15; z <- 0 to 15) {
+		for (x <- 0 until 16; z <- 0 until 16) {
 			// val height = Math.floor(Math.random() * 15).asInstanceOf[Int]
-			val height = Math.floor(8f * perlin.getValue(xMin + xSize*(x / 16f), 0, zMin + zSize*(z / 16f))).asInstanceOf[Int]
-			val heightMaxed = 2 + (if (height > 14) 14 else height)
-			for (y <- 0 to heightMaxed)
-				cubeMap(coord(x, y, z)) = 1
+			val height = Math.floor(13f * perlin.getValue(xMin + xSize*(x / 16f), 0, zMin + zSize*(z / 16f))).asInstanceOf[Int]
+			val heightMinMaxed = Math.max(Math.min(height, 13), 2)
+			for (y <- 0 until heightMinMaxed)
+				cubeMap(coord(x, y, z)) = 2
+			cubeMap(coord(x, heightMinMaxed, z)) = 1
 		}
 		genDL()
 	}
 
-	case class Coord(x: Int, y: Int, z: Int,
+	case class Voxel(x: Int, y: Int, z: Int, blockType: Int,
 					 top: Boolean = true, left: Boolean = true, right: Boolean = true, bottom: Boolean = true,
 					 front: Boolean = true, back: Boolean = true) {
 		def isEmpty = !top && !left && !right && !bottom && !front && !back
 	}
 
-	private val displayList = ArrayBuffer[Coord]()
+	private val displayList = ArrayBuffer[Voxel]()
 
 	private def genDL() {
-		for (x <- 0 to 15; y <- 0 to 15; z <- 0 to 15) {
+		for (x <- 0 until 16; y <- 0 until 16; z <- 0 until 16) {
 			def isFilled(x: Int, y: Int, z: Int) = cubeMap(coord(x, y, z)) > 0
 			if (isFilled(x, y, z)) {
 				def onEdge(c: Int) = c == 0 || c == 15
 				def isEdgeBlock(x: Int, y: Int, z: Int) = onEdge(x) || onEdge(y) || onEdge(z)
 				if (isEdgeBlock(x, y, z)) {
-					displayList += Coord(x, y, z)
+					displayList += Voxel(x, y, z, cubeMap(coord(x, y, z)))
 				} else {
-					val coord = Coord(x, y, z,
+					val voxel = Voxel(x, y, z,
+						cubeMap(coord(x, y, z)),
 						!isFilled(x, y + 1, z),
 						!isFilled(x - 1, y, z),
 						!isFilled(x + 1, y, z),
@@ -56,15 +58,14 @@ class Chunk {
 						!isFilled(x, y, z - 1),
 						!isFilled(x, y, z + 1)
 					)
-					if (!coord.isEmpty)
-						displayList += coord
+					if (!voxel.isEmpty)
+						displayList += voxel
 				}
 			}
 		}
-		println(displayList.length, displayList)
 	}
 
-	private def oneCube(xcen: Float, ycen: Float, zcen: Float, coord: Coord) {
+	private def oneCube(xcen: Float, ycen: Float, zcen: Float, voxel: Voxel) {
 		glEnable(GL_TEXTURE_2D)
 		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE)
 
@@ -74,8 +75,11 @@ class Chunk {
 		glScalef(0.5f,0.5f,0.5f)
 		glColor4f(1f, 1f, 1f, 1f)
 		//glBegin(GL_QUADS)
-		if (coord.top) {
-			glBindTexture(GL_TEXTURE_2D, Textures.grassTop)
+		if (voxel.top) {
+			voxel.blockType match {
+				case 1 => glBindTexture(GL_TEXTURE_2D, Textures.grassTop)
+				case 2 => glBindTexture(GL_TEXTURE_2D, Textures.dirt)
+			}
 			glBegin(GL_QUADS)
 			// glColor4f(1.0f,1.0f,0.0f, alpha)
 			glTexCoord2f(1f, 0f)
@@ -88,7 +92,7 @@ class Chunk {
 			glVertex3f( 1.0f, 1.0f, 1.0f)
 			glEnd()
 		}
-		if (coord.bottom) {
+		if (voxel.bottom) {
 			glBindTexture(GL_TEXTURE_2D, Textures.dirt)
 			glBegin(GL_QUADS)
 			// glColor4f(1.0f,0.5f,0.0f, alpha)
@@ -102,8 +106,11 @@ class Chunk {
 			glVertex3f( 1.0f,-1.0f,-1.0f)
 			glEnd()
 		}
-		if (coord.back) {
-			glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+		if (voxel.back) {
+			voxel.blockType match {
+				case 1 => glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+				case 2 => glBindTexture(GL_TEXTURE_2D, Textures.dirt)
+			}
 			glBegin(GL_QUADS)
 			// glColor4f(1.0f,0.0f,0.0f, alpha)
 			glTexCoord2f(1f, 0f)
@@ -116,22 +123,28 @@ class Chunk {
 			glVertex3f( 1.0f,-1.0f, 1.0f)
 			glEnd()
 		}
-		if (coord.front) {
-			glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+		if (voxel.front) {
+			voxel.blockType match {
+				case 1 => glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+				case 2 => glBindTexture(GL_TEXTURE_2D, Textures.dirt)
+			}
 			glBegin(GL_QUADS)
 			// glColor4f(1.0f,1.0f,0.0f, alpha)
-			glTexCoord2f(1f, 0f)
-			glVertex3f( 1.0f,-1.0f,-1.0f)
-			glTexCoord2f(0f, 0f)
-			glVertex3f(-1.0f,-1.0f,-1.0f)
-			glTexCoord2f(0f, 1f)
-			glVertex3f(-1.0f, 1.0f,-1.0f)
 			glTexCoord2f(1f, 1f)
+			glVertex3f( 1.0f,-1.0f,-1.0f)
+			glTexCoord2f(0f, 1f)
+			glVertex3f(-1.0f,-1.0f,-1.0f)
+			glTexCoord2f(0f, 0f)
+			glVertex3f(-1.0f, 1.0f,-1.0f)
+			glTexCoord2f(1f, 0f)
 			glVertex3f( 1.0f, 1.0f,-1.0f)
 			glEnd()
 		}
-		if (coord.left) {
-			glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+		if (voxel.left) {
+			voxel.blockType match {
+				case 1 => glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+				case 2 => glBindTexture(GL_TEXTURE_2D, Textures.dirt)
+			}
 			glBegin(GL_QUADS)
 			// glColor4f(0.0f,0.0f,1.0f, alpha)
 			glTexCoord2f(1f, 0f)
@@ -144,8 +157,11 @@ class Chunk {
 			glVertex3f(-1.0f,-1.0f, 1.0f)
 			glEnd()
 		}
-		if (coord.right) {
-			glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+		if (voxel.right) {
+			voxel.blockType match {
+				case 1 => glBindTexture(GL_TEXTURE_2D, Textures.grassSide)
+				case 2 => glBindTexture(GL_TEXTURE_2D, Textures.dirt)
+			}
 			glBegin(GL_QUADS)
 			// glColor4f(1.0f,0.0f,1.0f, alpha)
 			glTexCoord2f(1f, 0f)
